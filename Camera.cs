34535e55116d92e5c29ba7a1a7d269726e2fece0
Patategrill <194829329+patategrill@users.noreply.github.com>
2026-06-05 17:53:04 +0200

    Create Camera.cs

---
+++ Camera.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using unityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    /// fonction qui permet de faire suivre la caméra au joueur
+    public Transform player;
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, -10f);
+    }
+}

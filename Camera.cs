67006bf256a030707ad4cfc1bc7a5524dddade74
Patategrill <194829329+patategrill@users.noreply.github.com>
2026-06-05 23:46:04 +0200

    Update Camera.cs

--- Camera.cs
+++ Camera.cs
@@ -1,10 +1,15 @@
-using unityEngine;
+using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
 
     /// fonction qui permet de faire suivre la caméra au joueur
     public Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("player").transform;
+    }
 
     void LateUpdate()
     {
